272d17eda3edc8301991d6ea74cbfea6d8d9835d
Bismark <bettkipkoech45@gmail.com>
2025-03-11 18:00:44 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newCustomer) {
+  bestCustomer = newCustomer;
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  throw new Error('Assignment to constant variable.');
+}
